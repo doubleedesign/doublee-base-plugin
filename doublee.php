c81cc87bdc86fef0d847d57b9cef650ab66ecd82
Leesa Ward <leesa@doubleedesign.com.au>
2024-10-19 13:08:06 +1100

    Version bump

--- doublee.php
+++ doublee.php
@@ -5,7 +5,7 @@
  *
  * Author:      		Leesa Ward
  * Author URI:  		https://www.doubleedesign.com.au
- * Version:     		2.1.1
+ * Version:     		3.0.0
  * Requires at least: 	6.3.2
  * Requires PHP: 		8.1.9
  * Text Domain: 		doublee
