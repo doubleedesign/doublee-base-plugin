9e08df711f35881e9c5165db5068d009e14642dc
Leesa Ward <leesa@doubleedesign.com.au>
2025-02-23 14:21:18 +1100

    Remove ACF Extended stuff (didn't end up using it)

--- includes/class-admin-ui.php
+++ includes/class-admin-ui.php
@@ -22,10 +22,7 @@ class Doublee_Admin_UI {
         add_filter('acf/settings/enable_post_types', '__return_false');
         add_filter('acf/settings/enable_options_pages_ui', '__return_false');
 
-        // Specify which ACF Extended features to enable and disable
-        add_filter('acfe/init', array($this, 'select_acfe_features'));
-        add_filter('acf/get_field_types', array($this, 'disable_some_acfe_fields'));
-        // Also disable some core ACF fields
+        // Also Disable some core ACF fields
         add_filter('acf/get_field_types', array($this, 'disable_some_acf_fields'));
 
         // General admin screen customisations
@@ -131,31 +128,6 @@ class Doublee_Admin_UI {
 		}
 	}
 
-
-    /**
-     * Customise which ACF Extended modules are enabled
-     * Find the full list in the load() function in /plugins/acf-extended/acf-extended.php
-     * @since 3.0.0
-     */
-    function select_acfe_features(): void {
-        $modules_to_disable = ['author', 'categories', 'block_types', 'forms', 'forms/top_level', 'options', 'options_pages', 'post_types', 'taxonomies', 'multilang', 'performance'];
-        foreach ($modules_to_disable as $module) {
-            acf_update_setting("acfe/modules/$module", false);
-        }
-
-        $recaptcha_fields = ['site_key', 'secret_key', 'version', 'v2/theme', 'v2/size', 'v2/hide_logo'];
-        foreach ($recaptcha_fields as $field) {
-            acf_update_setting("acfe/forms/recaptcha/$field", false);
-        }
-
-        // Dev mode
-        acf_update_setting('acfe/dev', false); // TODO: How to turn this on but limit it to admins?
-
-        // UI enhancements
-        acf_update_setting('acfe/ui', true);
-    }
-
-
     /**
      * Disable some ACF fields
      * @since 3.0.0
@@ -168,33 +140,6 @@ class Doublee_Admin_UI {
         $disable = array(
             'Basic' => array('password'),
             'Advanced' => array('icon_picker', 'color_picker')
-        );
-
-        foreach ($disable as $category => $fields) {
-            foreach ($fields as $field) {
-                unset($field_types[$category][$field]);
-            }
-        }
-
-        return $field_types;
-    }
-
-
-    /**
-     * Disable some fields added by ACF Extended
-     * @since 3.0.0
-     *
-     * @param $field_types
-     *
-     * @return array
-     */
-    function disable_some_acfe_fields($field_types): array {
-        $disable = array(
-            'Basic' => array('acfe_button', 'acfe_hidden', 'acfe_slug'),
-            'Content' => array('acfe_code_editor'),
-            'Relational' => array('acfe_advanced_link', 'acfe_forms', 'acfe_taxonomy_terms'),
-            'Advanced' => array('acfe_recaptcha'),
-            'WordPress' => array('acfe_post_statuses', 'acfe_post_types', 'acfe_taxonomies', 'acfe_user_roles')
         );
 
         foreach ($disable as $category => $fields) {
